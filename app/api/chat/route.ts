72d18e95d355d5a93c351b22fb62c5ec7458c8db
Natnael Getahun <connect@ngetahun.me>
2024-09-28 00:15:28 +0200

    Fix missing user key from chat route

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: Request) {
   })
   const json = await req.json()
   const { messages, previewToken } = json
-  const userId = (await auth({ cookieStore }))?.user.id
+  const userId = (await auth({ cookieStore }))?.id
 
   if (!userId) {
     return new Response('Unauthorized', {
